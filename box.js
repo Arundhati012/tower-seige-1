4a850e41247017aa07398960938517ca1381e2b0
Arundhati012 <78207187+Arundhati012@users.noreply.github.com>
2021-04-23 12:05:25 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,10 @@
+class box {
+    constructor(bodyA,pointB){
+        var options = (
+            bodyA:bodyA;
+         pointB:pointB;
+         stiffness:0.04;
+         length:10;
+    }
+      
+}
